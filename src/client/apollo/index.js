d00d9e376467e2899da2754d01c78731ef581a1e
al ostrowski <110755768+palomasoftware@users.noreply.github.com>
2023-10-11 21:06:53 -0400

    Update index.js

--- src/client/apollo/index.js
+++ src/client/apollo/index.js
@@ -1,8 +1,8 @@
-import { ApolloClient } from 'apollo-client';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { onError } from 'apollo-link-error';
-import { ApolloLink } from 'apollo-link';
+import { ApolloClient } from '@apollo-client';
+import { InMemoryCache } from '@apollo-cache-inmemory';
+import { HttpLink } from '@apollo-link-http';
+import { onError } from '@apollo-link-error';
+import { ApolloLink } from '@apollo-link';
 import { gql } from '@apollo/client';
 
 
